62f5c293e4e05fe2d52b7d6ed51b1ca5b9308be1
MargaritaSv <margarita.esti@gmail.com>
2015-12-03 22:27:49 +0200

    Second homework - Exception_Handling

---
+++ Exception_Handling/EnterNumbers/EnterNumbers.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace EnterNumbers
+{
+    class EnterNumbers
+    {
+        static void Main()
+        {
+            int[] nums = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                int maxEls = nums.Max();
+                int min = Math.Min(2,maxEls+1);
+                int max = 100 - 10 + i;
+
+                Console.WriteLine("Enter the number: ");
+                nums[i] = ReadNumber(min,max);
+            }
+
+        }
+
+        public static int ReadNumber(int strat, int end)
+        {
+            string input = Console.ReadLine();
+            try
+            {
+                int num = int.Parse(input);
+                if (num < strat || num > end)
+                {
+                    throw new ArgumentOutOfRangeException("number", string.Format("Number should be between {0} and {1}", strat, end));
+                }
+
+                return num;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Number should be in the range [{0} .... {1}].Enter again:",strat,end);
+                return ReadNumber(strat, end);
+            }
+
+        }
+    }
+}
---
+++ Exception_Handling/SquareRoot/SquareRoot.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SquareRoot
+{
+    class SquareRoot
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the integer number: ");
+
+            try
+            {
+                string input = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    throw new ArgumentNullException("input", "Input canot be empty.");
+                }
+                int num = int.Parse(input);
+                if (num < 0)
+                {
+                    throw new ArgumentOutOfRangeException("number", "The number must be positive");
+                }
+                Console.WriteLine("The sqare root of {0} is {1:F2}", num, Math.Sqrt(num));
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Invalid number", ex.Message);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Invalid number -the number cannot be letter for example", ex.Message);
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye!");
+            }
+        }
+
+    }
+}
